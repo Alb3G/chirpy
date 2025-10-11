1e70f6816dcc280851898856e579dbb85bc95fde
Alb3G <guzmanmorenoalberto1998@gmail.com>
2025-10-11 18:21:16 +0200

    feat: :sparkles: Added chirp handler that validates JSON body
    Added chirp handler that validates JSON body

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync/atomic"
@@ -8,6 +9,18 @@ import (
 
 type apiConfig struct {
 	fileserverhits atomic.Int32
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+type SuccessResponse struct {
+	Valid bool `json:"valid"`
+}
+
+type ChirpBody struct {
+	Body string `json:"body"`
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,10 +35,43 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
 
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Header.Get("Content-Type") != "application/json" {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ErrorResponse{Error: "Something went wrong"})
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+
+	var body ChirpBody
+	decoder.Decode(&body)
+
+	if len(body.Body) > 140 {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ErrorResponse{Error: "Chirp is too long"})
+		return
+	}
+
+	json.NewEncoder(w).Encode(SuccessResponse{Valid: true})
+}
+
 func (ac *apiConfig) hitsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hits: %d", ac.fileserverhits.Load())
+
+	template := `<html>
+  		<body>
+    		<h1>Welcome, Chirpy Admin</h1>
+    		<p>Chirpy has been visited %d times!</p>
+  		</body>
+	</html>`
+
+	fmt.Fprintf(w, template, ac.fileserverhits.Load())
 }
 
 func (ac *apiConfig) metricsCountMiddleware(next http.Handler) http.Handler {
--- main.go
+++ main.go
@@ -16,9 +16,10 @@ func main() {
 
 	apiCfg := &apiConfig{}
 
-	mux.HandleFunc("GET /healthz", healthHandler)
-	mux.HandleFunc("GET /metrics", apiCfg.hitsHandler)
-	mux.HandleFunc("POST /reset", apiCfg.resetHitsHandler)
+	mux.HandleFunc("GET /api/healthz", healthHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.hitsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHitsHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 
 	fileServer := http.FileServer(http.Dir(FILE_PATH_ROOT))
 	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.metricsCountMiddleware(fileServer)))
