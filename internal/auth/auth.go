41de266584917ab2072259e15106b097511c468c
Alb3G <guzmanmorenoalberto1998@gmail.com>
2025-10-14 17:18:58 +0200

    feat: :sparkles: Added 2 methods along with test cases for creating and validating JWT
    Added 2 methods along with test cases for creating and validating JWT

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,12 @@
 package auth
 
-import "github.com/alexedwards/argon2id"
+import (
+	"time"
+
+	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
 
 func HashPassword(password string) (string, error) {
 	hashed_pass, err := argon2id.CreateHash(password, argon2id.DefaultParams)
@@ -14,4 +20,39 @@ func HashPassword(password string) (string, error) {
 func CheckPasswordHash(password, hash string) (bool, error) {
 	match, _, err := argon2id.CheckHash(password, hash)
 	return match, err
+}
+
+func MakeJWT(userId uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	claims := jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  &jwt.NumericDate{Time: time.Now().UTC()},
+		ExpiresAt: &jwt.NumericDate{Time: time.Now().Add(expiresIn)},
+		Subject:   userId.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedToken, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, &claims, func(t *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	userId, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return uuid.MustParse(userId), nil
 }
--- testing/auth_test.go
+++ testing/auth_test.go
@@ -2,8 +2,10 @@ package testing
 
 import (
 	"testing"
+	"time"
 
 	auth "github.com/Alb3G/chirpy/internal/auth"
+	"github.com/google/uuid"
 )
 
 func TestPasswordHashing(t *testing.T) {
@@ -37,5 +39,30 @@ func TestCheckHash(t *testing.T) {
 
 	if !match {
 		t.Fatalf("Hash and password should match")
+	}
+}
+
+func TestMakeAndValidateJWT(t *testing.T) {
+	// Creation of JWT
+	userId := uuid.MustParse("fb68025f-be8f-4649-aa15-0c2b6b1c6409")
+	secret := "test-secret-key"
+
+	token, err := auth.MakeJWT(userId, secret, time.Minute*2)
+	if err != nil {
+		t.Fatalf("Failed signing JWT: %v", err)
+	}
+
+	if token == "" {
+		t.Error("Expected non-empty token value")
+	}
+
+	// Validation of JWT
+	validatedUserId, err := auth.ValidateJWT(token, secret)
+	if err != nil {
+		t.Fatalf("Internal error validation of the JWT, err: %v", err)
+	}
+
+	if validatedUserId.String() != userId.String() {
+		t.Error("Error expecting token values to be equals")
 	}
 }
